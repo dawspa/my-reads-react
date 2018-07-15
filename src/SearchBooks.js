900c239d35ca7cc4969bc18e2b5b0e439c2dee22
dawid <dejw88@gmail.com>
2018-07-16 01:05:59 +0200

    moved functionality to components

---
+++ src/SearchBooks.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import Shelf from './Bookshelves'
+
+class SearchBooks extends Component {
+
+  render() {
+    const {booksFiltered, booksSearch, updateOption} = this.props
+
+    return (
+      <div>
+        <div className="search-books-bar">
+          <Link to='/' className='close-search'>Close</Link>
+          <div className='search-books-input-wrapper'>
+            <input
+              className='search-books-results'
+              type='text'
+              placeholder='Search books..'
+              onChange={(event) => booksSearch(event.target.value)}/>
+          </div>
+        </div>
+
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {booksFiltered.map(book => (<Shelf book={book} key={book.id} updateOption={updateOption}/>))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+export default SearchBooks
